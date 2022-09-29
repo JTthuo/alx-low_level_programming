c021b0bba78989b7d9e1b551dcd4fd2b06317c28
JTthuo <josephnthuo9@gmail.com>
2022-09-29 13:59:38 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,9 +2,7 @@
 
 /**
  * _puts_recursion - prints's a string followed by a new line
- * @s: string to be printed
- *
- * Return: void
+ * @s: string to be print
  */
 
 void _puts_recursion(char *s)
@@ -12,10 +10,8 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
-	else
-	{
 		_putchar(s[0]);
 		_puts_recursion(s + 1);
-	}
 }
