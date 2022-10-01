166526378c79e145c968b5f78e9ed7b93329ef9f
JTthuo <josephnthuo9@gmail.com>
2022-09-30 17:12:56 -0700

    task 1

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strchr - locates a character in a string
+ * in function _strchr - locates a character in a string
  * @s: string to check
  * @c: character to check for
  *
@@ -15,8 +15,10 @@ char *_strchr(char *s, char c)
 {
 	if (*(s + i) == c)
 		return (s + i);
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		return (0);
+	}
 }
-if (*(s + i) == c)
-	return (s + i);
-	return (0);
 }
