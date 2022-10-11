7dbe385f3596f17e50030b7f0567bfabac7f4978
JTthuo <josephnthuo9@gmail.com>
2022-10-11 06:32:40 -0700

    0-create_array.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,14 +1,15 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
+#include <stdlib.h>
+
+int _putchar(char);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
-int _strlen(char *s);
 char *str_concat(char *s1, char *s2);
-char *_strncat(char *dest, char *src, int n);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
-#endif /*MAIN.H*/
+#endif /* MAIN_H */
